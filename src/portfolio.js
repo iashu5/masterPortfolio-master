1aeba7124025493ae23e6042edc63a25a5f111d0
ASHUPC\DELL <ashutosh.kumar@medimaze.in>
2025-05-16 11:09:58 +0530

    resume changed

--- src/portfolio.js
+++ src/portfolio.js
@@ -24,7 +24,7 @@ const greeting = {
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
   resumeLink:
-    "https://drive.google.com/file/d/1utQ4vSpz5g2UVY5B23cPaOI6uy9jNlHN/view?usp=sharing",
+    "https://drive.google.com/file/d/1_tfucq_jnnhwFTUpJ4dXjNWUXBdT4AfN/view?usp=sharing",
   portfolio_repository: "https://ashucodes.netlify.app/",
   githubProfile: "https://github.com/iashu5",
 };
